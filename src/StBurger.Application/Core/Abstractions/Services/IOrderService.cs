ad44621afa4778d1347d8e8020096a5e10a13edd
Weslley Murdock <weslleyluiz@outlook.com.br>
2026-04-21 18:12:07 -0300

    fix method to OrderService

--- src/StBurger.Application/Core/Abstractions/Services/IOrderService.cs
+++ src/StBurger.Application/Core/Abstractions/Services/IOrderService.cs
@@ -4,8 +4,8 @@ public interface IOrderService
 {
     Task<OrderResponse> AddItemAsync(string id, string itemId, CancellationToken cancellationToken);
     Task<CreateOrderResponse> CreateOrderAsync(CreateOrderRequest data, CancellationToken cancellationToken);
-    Task<Unit> DeleteOrderAsync(string id, CancellationToken cancellationToken);
     Task<Unit> DeleteItemAsync(string id, string itemId, CancellationToken cancellationToken);
+    Task<Unit> DeleteOrderAsync(string id, CancellationToken cancellationToken);
     Task<IList<OrderResponse>> GetAsync(CancellationToken cancellationToken);
     Task<OrderResponse?> GetAsync(string id, CancellationToken cancellationToken);
     Task<UpdateOrderResponse> UpdateOrderAsync(UpdateOrderRequest data, CancellationToken cancellationToken);
