5973afc28ec9e44da5345fcb3de82d9c6cdc8671
Weslley Murdock <weslleyluiz@outlook.com.br>
2026-04-21 05:41:25 -0300

    Add infrastructure repositories

---
+++ src/StBurger.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,66 @@
+﻿using StBurger.Application.Core.Abstractions.Repositories;
+using StBurger.Domain.Core.Entities;
+using StBurger.Infrastructure.Persistence;
+namespace StBurger.Infrastructure.Repositories;
+using Domain.Core.Exceptions;
+
+public class Repository<T, TId>(StBurgerDbContext dbContext) : IRepository<T, TId> where T : AuditableEntity<TId>, IAuditableEntity<TId>
+    where TId : class, IEquatable<TId>
+{
+    public IQueryable<T> Entities => dbContext.Set<T>();
+
+    public async Task<T> AddAsync(T entity)
+    {
+        await dbContext.Set<T>().AddAsync(entity);
+        return entity;
+    }
+
+    public async Task DeleteAsync(T entity)
+    {
+        await dbContext.Set<T>().Where(x => x.Id == entity.Id).ExecuteDeleteAsync<T>();
+    }
+
+    public async Task<IList<T>> GetAllAsync()
+    {
+        return await dbContext
+            .Set<T>()
+            .ToListAsync();
+    }
+
+    public async Task<T> GetByIdAsync(TId id)
+    {
+        return await dbContext.Set<T>().FindAsync(id) ?? throw new EntityNotFoundException($"Entity not found", "NOT_FOUND");
+    }
+
+    public async Task<IList<T>> GetPagedResponseAsync(int pageNumber, int pageSize)
+    {
+        if (pageNumber <= 0)
+        {
+            throw new ArgumentException("Page number must be greater than zero.", nameof(pageNumber));
+        }
+        if (pageSize <= 0)
+        {
+            throw new ArgumentException("Page size must be greater than zero.", nameof(pageSize));
+        }
+
+        var toSkip = (pageNumber - 1) * pageSize;
+
+        if (dbContext.Set<T>().Count() <= toSkip)
+        {
+            throw new ArgumentException("Page number exceeds total pages.", nameof(pageNumber));
+        }
+        return await dbContext
+            .Set<T>()
+            .Skip(toSkip)
+            .Take(pageSize)
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        T exist = await dbContext.Set<T>().FindAsync(entity.Id) ?? throw new EntityNotFoundException($"Entity not found", "NOT_FOUND");
+        dbContext.Entry(exist).CurrentValues.SetValues(entity);
+        
+    }
+}
---
+++ src/StBurger.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,71 @@
+﻿using StBurger.Application.Core.Abstractions.Repositories;
+using StBurger.Domain.Core.Entities;
+using StBurger.Infrastructure.Persistence;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StBurger.Infrastructure.Repositories
+{
+    public class UnitOfWork<TId> : IUnitOfWork<TId> where TId : class, IEquatable<TId>
+    {
+        private readonly StBurgerDbContext _dbContext;
+        private bool disposed;
+        private Hashtable _repositories;
+
+        public UnitOfWork(StBurgerDbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public IRepository<TEntity, TId> Repository<TEntity>() where TEntity : AuditableEntity<TId>
+        {
+            _repositories ??= [];
+
+            var type = typeof(TEntity).Name;
+
+            if (!_repositories.ContainsKey(type))
+            {
+                var repositoryType = typeof(Repository<,>);
+
+                var repositoryInstance = Activator.CreateInstance(repositoryType.MakeGenericType(typeof(TEntity), typeof(TId)), _dbContext);
+
+                _repositories.Add(type, repositoryInstance);
+            }
+
+            return (IRepository<TEntity, TId>)_repositories[type];
+        }
+
+        public async Task<int> Commit(CancellationToken cancellationToken)
+        {
+            return await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public Task Rollback()
+        {
+            _dbContext.ChangeTracker.Entries().ToList().ForEach(x => x.Reload());
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    //dispose managed resources
+                    _dbContext.Dispose();
+                }
+            }
+            //dispose unmanaged resources
+            disposed = true;
+        }
+    }
+}
