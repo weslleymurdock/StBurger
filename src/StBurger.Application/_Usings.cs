59ea148b67102fe8131205c38661c7acfb1e0b7d
Weslley Murdock <weslleyluiz@outlook.com.br>
2026-04-21 05:39:35 -0300

    Add remaining resources from application

---
+++ src/StBurger.Application/_Usings.cs
@@ -0,0 +1,20 @@
+﻿global using FluentValidation;
+
+global using MediatR;
+
+global using StBurger.Application.Core.Abstractions.Services;
+global using StBurger.Application.Core.Abstractions.Repositories;
+
+global using StBurger.Application.Menu.Commands;
+global using StBurger.Application.Menu.Queries;
+global using StBurger.Application.Menu.Requests;
+global using StBurger.Application.Menu.Responses;
+
+global using StBurger.Application.Order.Commands;
+global using StBurger.Application.Order.Queries;
+global using StBurger.Application.Order.Requests;
+global using StBurger.Application.Order.Responses;
+
+global using StBurger.Domain.Orders.Entities;
+
+
