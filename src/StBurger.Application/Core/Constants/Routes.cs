5698ca3f781ef57d8e1d1c0949cedd49e6f20c41
Weslley Murdock <weslleyluiz@outlook.com.br>
2026-04-21 05:30:50 -0300

    Add application core

---
+++ src/StBurger.Application/Core/Constants/Routes.cs
@@ -0,0 +1,22 @@
+﻿namespace StBurger.Application.Core.Constants;
+
+public static class Routes
+{
+    public static class Base
+    {
+        public const string Api = "/api";
+        public const string Version = "/v1";
+    }
+
+    public static class Menu
+    {
+        public const string Base = $"{Routes.Base.Api}{Routes.Base.Version}/menu";
+        public const string Tag = "Catalog";
+    }
+
+    public static class Order
+    {
+        public const string Base = $"{Routes.Base.Api}{Routes.Base.Version}/order";
+        public const string Tag = "Orders";
+    }
+}
---
+++ src/StBurger.Application/Core/Responses/BaseResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace StBurger.Application.Core.Responses;
+
+public class BaseResponse
+{
+    public int Code { get; set; } = 200;
+    public string? Message { get; set; } = string.Empty;
+}
+
+public class BaseResponse<T>(T data) : BaseResponse where T : class
+{
+    public T? Data { get; set; } = data;
+}
