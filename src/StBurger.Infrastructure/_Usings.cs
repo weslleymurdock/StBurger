1d8848c133177a9b780af7309ea6c63c7f0c8142
Weslley Murdock <weslleyluiz@outlook.com.br>
2026-04-21 05:49:17 -0300

    Add remaining infrastructure assets

---
+++ src/StBurger.Infrastructure/_Usings.cs
@@ -0,0 +1,9 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+global using StBurger.Application.Core.Enums;
+global using StBurger.Domain.Core.Abstractions;
+global using StBurger.Domain.Menu.Entities;
+global using StBurger.Domain.Orders.Entities;
+global using StBurger.Infrastructure.Persistence.Models;
