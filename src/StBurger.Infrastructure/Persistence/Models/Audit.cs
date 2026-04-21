8a13767a069359317310984491d27e05999fa992
Weslley Murdock <weslleyluiz@outlook.com.br>
2026-04-21 05:28:08 -0300

    Add Infrrastructure Models

---
+++ src/StBurger.Infrastructure/Persistence/Models/Audit.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using StBurger.Domain.Core.Abstractions;
+
+namespace StBurger.Infrastructure.Persistence.Models;
+
+public class Audit : IEntity<string>
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string Type { get; set; } = string.Empty;
+    public string TableName { get; set; } = string.Empty;
+    public DateTime DateTime { get; set; } = DateTime.UtcNow;
+    public string OldValues { get; set; } = string.Empty;
+    public string NewValues { get; set; } = string.Empty;
+    public string AffectedColumns { get; set; } = string.Empty;
+    public string PrimaryKey { get; set; } = string.Empty;
+}
---
+++ src/StBurger.Infrastructure/Persistence/Models/AuditEntry.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Newtonsoft.Json;
+using StBurger.Application.Core.Enums;
+
+namespace StBurger.Infrastructure.Persistence.Models;
+
+public class AuditEntry(EntityEntry entry = default!)
+{
+    public EntityEntry Entry { get; } = entry;
+    public string TableName { get; set; } = string.Empty;
+    public Dictionary<string, object> KeyValues { get; } = [];
+    public Dictionary<string, object> OldValues { get; } = [];
+    public Dictionary<string, object> NewValues { get; } = [];
+    public List<PropertyEntry> TemporaryProperties { get; } = [];
+    public AuditType AuditType { get; set; } = AuditType.None;
+    public List<string> ChangedColumns { get; } = [];
+    public bool HasTemporaryProperties => TemporaryProperties.Any();
+
+    public Audit ToAudit()
+    {
+        var audit = new Audit
+        {
+            Type = AuditType.ToString(),
+            TableName = TableName,
+            DateTime = DateTime.UtcNow,
+            PrimaryKey = JsonConvert.SerializeObject(KeyValues),
+            OldValues = OldValues.Count == 0 ? null : JsonConvert.SerializeObject(OldValues),
+            NewValues = NewValues.Count == 0 ? null : JsonConvert.SerializeObject(NewValues),
+            AffectedColumns = ChangedColumns.Count == 0 ? null : JsonConvert.SerializeObject(ChangedColumns)
+        };
+        return audit;
+    }
+}
