21ea477bbcb0d979f95b3eb71b7545dea6fcc25d
Weslley Murdock <weslleyluiz@outlook.com.br>
2026-04-22 23:44:48 -0300

    Improve response and refactors

--- src/StBurger.Application/Menu/Responses/MenuItemResponse.cs
+++ src/StBurger.Application/Menu/Responses/MenuItemResponse.cs
@@ -2,29 +2,29 @@
 
 namespace StBurger.Application.Menu.Responses;
 
-public record MenuItemResponse(string Name, decimal Price, string Description)
+public record MenuItemResponse(string Id, string Name, decimal Price, string Description)
 {
     internal static MenuItemResponse FromEntity(MenuItem item)
     {
         ArgumentNullException.ThrowIfNull(item, nameof(item));
-        return new MenuItemResponse(item.Name, item.Price, item.Description);
+        return new MenuItemResponse(item.Id, item.Name, item.Price, item.Description);
     }
 
     internal static MenuItemResponse FromEntity(OrderItem item)
     {
         ArgumentNullException.ThrowIfNull(item, nameof(item));
-        return new MenuItemResponse(item.MenuItem.Name, item.MenuItem.Price, item.MenuItem.Description);
+        return new MenuItemResponse(item.MenuItem.Id, item.MenuItem.Name, item.MenuItem.Price, item.MenuItem.Description);
     }
 
     internal static MenuItemResponse FromOrderItemResponse(OrderItem item)
     {
         ArgumentNullException.ThrowIfNull(item, nameof(item));
-        return new MenuItemResponse(item.MenuItem.Name, item.MenuItem.Price, item.MenuItem.Description);
+        return new MenuItemResponse(item.MenuItem.Id, item.MenuItem.Name, item.MenuItem.Price, item.MenuItem.Description);
     }
 
     internal static MenuItemResponse ToResponse(MenuItem item)
     {
         ArgumentNullException.ThrowIfNull(item, nameof(item));
-        return new MenuItemResponse(item.Name, item.Price, item.Description);
+        return new MenuItemResponse(item.Id, item.Name, item.Price, item.Description);
     }
 }
